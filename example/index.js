b70f840c4c07e3d606595f9384500b5e59e4d40c
Rodrigo Muniz <muniz95@bsd.com.br>
2018-03-29 15:58:50 +0000

    Login e página de assembleias funcionando

--- example/index.js
+++ example/index.js
@@ -2,4 +2,6 @@ const condovox = require("../dist");
 require('dotenv').config();
 
 const {user, pass} = process.env
-condovox.assembleias(user, pass).then(console.log);
+condovox.assembleias(user, pass).then((response) => {
+    console.log(response.body);
+});
--- src/index.js
+++ src/index.js
@@ -11,20 +11,7 @@ export const login = async (acesso_login, acesso_senha) => {
         acesso_submit: ''
     },
     resolveWithFullResponse: true,
-    simple: false,
-    headers: {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'pt-BR,pt;q=0.8,en-US;q=0.6,en;q=0.4,eo;q=0.2',
-        'Connection': 'keep-alive',
-        'Content-Length': '57',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Cookie': 'condovox=51f686e75df3e4e99c5574100bb30fc5',
-        'Host': 'www.spaziochampville.com.br',
-        'Referer': 'https://www.spaziochampville.com.br/',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0'
-    }
+    simple: false
   }
   const req = request.defaults({
     jar: true
@@ -37,11 +24,7 @@ export const login = async (acesso_login, acesso_senha) => {
 export const assembleias = (user, pass) => {
     const options = {
         uri: `${URL}/assembleias`,
-        resolveWithFullResponse: true,
-        simple: false,
-        headers: {
-            'Host': 'www.spaziochampville.com.br'
-        }
+        resolveWithFullResponse: true
     }
     return login(user, pass)
         .then(req => req.get(options))
