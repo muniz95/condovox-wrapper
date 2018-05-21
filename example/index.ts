9b3b4f31b1bef3cc85a80ee3fa5aae2d1bcc6e1d
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-05-21 15:05:54 -0300

    Código Javascript completamente removido do projeto

---
+++ example/index.ts
@@ -0,0 +1,20 @@
+import { config } from "dotenv";
+import { Condovox } from "../dist";
+
+(async () => {
+    config();
+
+    let {user, pass} = process.env;
+    if (user === undefined) {
+        user = "";
+    }
+    if (pass === undefined) {
+        pass = "";
+    }
+    const condovox = new Condovox(user, pass);
+    await condovox.login();
+    const assembleias = await condovox.listAssembleias();
+
+    // tslint:disable-next-line:no-console
+    console.log(assembleias);
+})();
